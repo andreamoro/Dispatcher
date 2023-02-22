74dbe9fb9916fa9c8705cae4384f73ea7729c9c0
Andrea Moro <info@andreamoro.eu>
2023-02-22 08:40:50 +0100

    revisited the testing routine for module testing

--- test.py
+++ test.py
@@ -6,37 +6,44 @@ def foo():
 
 @foo.register
 def _(dimension: str, expression: str, operator: str, append: bool):
+    print('')
     print("dimension: str, expression: str, operator: str, append: bool")
     print(f"dimension: {dimension}, expression: {expression}, operator: {operator}, append: {append}")    
 
 @foo.register
 def _(arg:int):
+    print('')
     print("arg:int")
     print(f"arg:{arg}")
     
 @foo.register
 def _(arg:list):
+    print('')
     print("arg:list")
     print(f"arg:{arg}")
     pass
 
-@foo.register
-def _(arg:str):
-    print("arg:str")
-    print(f"arg:{arg}")
+# @foo.register
+# def _(arg:str):
+#     print('')
+#     print("arg:str")
+#     print(f"arg:{arg}")
 
 @foo.register
 def _(country:str = ''):
+    print('')
     print("country:str")
     print(f"country:{country}")
 
 @foo.register
 def _(country, operator, append):
+    print('')
     print("country, operator, append")
     print(f"{country}, {operator}, {append}")
 
 @foo.register
 def _(country: str, operator: str = 'and', append: bool = False):
+    print('')
     print("country: str, operator: str = 'and', append: bool = False")
     print(f"country: {country}, operator: {operator}, append: {append}")
 
@@ -50,15 +57,14 @@ if __name__ == '__main__':
     # Expected to call arg:int - Dispatch to existing method
     # 
     foo(1)
-    
-    foo(1, 2, 3)    
-    foo('a', 'b', 'c')
         
     # Expected to call (country, operator, append)
     foo('a', 'b', 'c')
     foo(1, 2, 3)
 
-    # Expected to call (arg: str)
+    # Expected to call (arg: str) if not commented
+    # else (country: str, operator: str = 'and', append: bool = False)
+    # The two method cannot exist together
     foo('test') 
 
     # Expected to call (country: str)
